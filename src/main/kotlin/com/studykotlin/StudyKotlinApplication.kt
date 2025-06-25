e4423fad9e0bd57f73b6fa74ac84825a9cc0cedb
ssol-park <ksttlov.naver@hanmail.net>
2025-06-25 21:29:30 +0900

    initialize

---
+++ src/main/kotlin/com/studykotlin/StudyKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.studykotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/studykotlin/StudyKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.studykotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
